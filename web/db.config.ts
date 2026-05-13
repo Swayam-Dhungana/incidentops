939b130afcb8a5497d77d4246d9f52ee70cc295c
Swayam-Dhungana <swayamdhungana.official@gmail.com>
2026-05-13 10:31:56 +0530

    refactor(folders): Separation of the services
    Made separate folders for frontend and the backend services. The db folder has been kept outside for easier reach by other running services along with which monorepo folder for the project is maintained henceforth this commit

--- web/db.config.ts
+++
@@ -1,9 +0,0 @@
-import postgres from "postgres";
-const dbUrl=process.env.DATABASE_URL!;
-const sql = postgres(dbUrl, {
-  max: 10,
-  idle_timeout: 20,
-  connect_timeout: 30
-});
-
-export default sql;
