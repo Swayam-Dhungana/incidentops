3395710ee4eeac53bf0417ec8fdb08e379281dfa
Swayam-Dhungana <swayamdhungana.official@gmail.com>
2026-05-14 11:03:07 +0530

    feat(middleware): Added middleware for fetching user from the session_token

---
+++ apps/api/src/middlewares/requireAuthenticated.ts
@@ -0,0 +1,17 @@
+import { getCookie } from "hono/cookie";
+import { createMiddleware } from "hono/factory";
+import crypto from 'crypto'
+import sql from "../../db.config";
+export const getUserFromSession=createMiddleware(async(c,next)=>{
+    const session_token=getCookie(c,'session_token');
+    if(!session_token){
+        return c.json({success:false, message:'Authentication Failed', error:'Middleware redirected the request'})
+    }
+    const session_hash=crypto.createHash('sha256').update(session_token).digest('hex');
+    const userId=sql`select id from sessions where session_auth_hash=${session_hash}`;
+    if(!userId){
+        return c.json({success:false, message:'Authentication Failed', error:'Middleware redirected the request'})
+    }
+    c.set('userId',userId);
+    next();
+})
